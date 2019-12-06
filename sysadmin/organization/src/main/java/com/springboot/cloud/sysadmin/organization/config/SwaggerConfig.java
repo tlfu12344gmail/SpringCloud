6b3bdb5c5be286b3144187241294eb11d12d1d95
futl <futl@deepblueai.com>
2019-12-06 16:17:49 +0800

    优化公共nacos配置

--- sysadmin/organization/src/main/java/com/springboot/cloud/sysadmin/organization/config/SwaggerConfig.java
+++ sysadmin/organization/src/main/java/com/springboot/cloud/sysadmin/organization/config/SwaggerConfig.java
@@ -1,11 +1,19 @@
 package com.springboot.cloud.sysadmin.organization.config;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,14 +21,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    @Value("${swagger.host")
+    private String host;
     @Bean
     public Docket createRestApi() {
+        List<Parameter> pars = new ArrayList<>();
+        pars.add(headerParam("Authorization", "令牌", ""));
+        pars.add(headerParam("X-Accept-Locale", "语言", "zh_CN"));
         return new Docket(DocumentationType.SWAGGER_2)
+                .host(host)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.springboot.cloud.sysadmin.organization"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(pars)
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .useDefaultResponseMessages(false);
+
     }
 
     private ApiInfo apiInfo() {
@@ -31,5 +50,13 @@ public class SwaggerConfig {
                 .version("2.0")
                 .build();
     }
-
+    private Parameter headerParam(String name, String description, String defaultVal) {
+        return new ParameterBuilder()
+            .name(name)
+            .description(description)
+            .modelRef(new ModelRef("string"))
+            .defaultValue(defaultVal)
+            .parameterType("header")
+            .required(false).build();
+    }
 }
